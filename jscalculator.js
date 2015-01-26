6e66c77542215ff27e79fab49bcca29bb310b1b3
yusfer <yustasfernando@gmail.com>
2015-01-26 14:34:58 +0100

    Correcto el sumador simple

--- jscalculator.js
+++ jscalculator.js
@@ -1,10 +1,11 @@
 function changer (id, newValue) {
-		 var element = document.getElementById(id);
-		 element.innerHTML = newValue;
-	   }
-	   function adder (sumtag, restag) {
-		 var operation = document.getElementById(sumtag);
-		 var operands = operation.innerHTML.split ("+");
-		 var result = parseInt(operands[0]) + parseInt(operands[1]);
-		 changer (restag, '=' + result);
-	   }
+	var element = document.getElementById(id);
+	element.innerHTML = newValue;
+}
+
+function sumador (sumtag, restag) {
+	var arraysumandos = document.getElementById(sumtag);
+	var sumandos = arraysumandos.innerHTML.split ("+",2);
+	var res = parseInt(sumandos[0]) + parseInt(sumandos[1]);
+	changer (restag, '=' + res);
+}
